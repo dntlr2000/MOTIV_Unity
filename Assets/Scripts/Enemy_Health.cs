10babfee3212eede1bd497decedb379a77c3f749
dntlr2000 <60862907+dntlr2000@users.noreply.github.com>
2024-08-05 16:08:55 +0900

    Add files via upload

---
+++ Assets/Scripts/Enemy_Health.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_Health : MonoBehaviour
+{
+    public float maxHealth = 1000; //각 페이즈마다 체력을 다르게 설정해야 함
+    private float currentHealth;
+    public int life = 3;
+    private int currentLife;
+    public string damageTag = "P_Attack";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth = maxHealth;
+        currentLife = life;
+    }
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == damageTag)
+        {
+            damaged(5.0f);
+        }
+    }
+
+    public void damaged(float damage)
+    {
+        currentHealth -= damage;
+        Debug.Log($"Damaged: {damage}, Current Health: {currentHealth}");
+
+        if (currentHealth < 0)
+        {
+            life -= 1;
+            currentHealth = maxHealth;
+
+        }
+
+        if (life == 0) {
+            Debug.Log("Enemy Defeated");
+            gameObject.SetActive(false);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
